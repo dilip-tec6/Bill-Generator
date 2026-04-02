70189ee1ac5605c403589e4a702f3c62f92f06f6
Rameshbo2580 <rameshbohora4578@gmail.com>
2026-04-02 20:03:19 +0545

    All the methods are ready

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Pizza basePizza = new Pizza(true);
+        basePizza.addExtraToppings();
+        basePizza.addExtraCheese();
+        basePizza.takeAway();
+        basePizza.getBill();
+    }
+}
